944330a176c80451d142ab86b682bee822a64e42
Skolnieks <skolnieks.pjvt@pjvt.local>
2025-03-05 13:44:51 +0200

    you better work

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('style.css') }}">
 </head>
 <body>
-    <h1>hi<h1>
+<header class="slay-queen-header">
+    <h1>Slay Queen!</h1>
+    <p>You better work, b****</p>
+    <img src="ru.gif" />
+  </header>
 </body>
 </html>
