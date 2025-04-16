9ef4a2c373c58322d48f2f4314f441f19a4abc5f
Skolnieks <skolnieks.pjvt@pjvt.local>
2025-04-16 16:01:55 +0300

    jekabscharles

--- app/Models/ToDo.php
+++ app/Models/ToDo.php
@@ -5,8 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-
 class ToDo extends Model
 {
+  use HasFactory;
   protected $fillable = ["content", "completed"];
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\ToDo;
+use App\Models\Diary;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,6 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         ToDo::factory()->count(10)->create();
+        Diary::factory()->count(15)->create();
 
       
     }
